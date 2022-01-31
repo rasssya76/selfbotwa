f4df7b5bf098a9d934793f2c05ad299d07f9394d
rasssya76 <80687852+rasssya76@users.noreply.github.com>
2022-01-31 15:25:23 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -498,6 +498,7 @@ if (!mek.key.fromMe && banChats === true) return
 switch (command) {
  
    case 'menu':
+   case 'anu':
     case 'help':
     var p = '```'
     run = process.uptime() 
@@ -943,6 +944,7 @@ const pebz3 = {
 			})
 			break
 	case 'ytsearch':
+	case 'yts':
 			if (args.length < 1) return reply('Tolong masukan query!')
 			var srch = args.join('');
 			try {
@@ -1144,20 +1146,13 @@ result = `❒「  *Wiki*  」
 		break
 		case 'hidetag':
      	 if (!mek.key.fromMe)            	
-     	 var value = args.join(' ')
-		var group = await pebz.groupMetadata(from)
-		var member = group['participants']
-		var mem = []
-    	member.map(async adm => {
-		mem.push(adm.id.replace('c.us', 's.whatsapp.net'))
-		})
-		var optionshidetag = {
-	    text: value,
-		contextInfo: { mentionedJid: mem },
-		quoted: mek
-		}
-	    pebz.sendMessage(from, optionshidetag, text, { quoted: { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(from ? { remoteJid: "393470602054-1351628616@g.us" } : {}) }, message: { "imageMessage": { "url": "https://mmg.whatsapp.net/d/f/At0x7ZdIvuicfjlf9oWS6A3AR9XPh0P-hZIVPLsI70nM.enc", "mimetype": "image/jpeg", "caption":'LordItsPebri',"fileSha256": "+Ia+Dwib70Y1CWRMAP9QLJKjIJt54fKycOfB2OEZbTU=", "fileLength": "28777", "height": 1080, "width": 1079, "mediaKey": "vXmRR7ZUeDWjXy5iQk17TrowBzuwRya0errAFnXxbGc=", "fileEncSha256": "sR9D2RS5JSifw49HeBADguI23fWDz1aZu4faWG/CyRY=", "directPath": "/v/t62.7118-24/21427642_840952686474581_572788076332761430_n.enc?oh=3f57c1ba2fcab95f2c0bb475d72720ba&oe=602F3D69", "mediaKeyTimestamp": "1610993486", "jpegThumbnail": gambar} }  } })
-					break
+     	 ht = body.slice(9)
+                members_id = []
+				for (let mem of groupMembers) {
+					members_id.push(mem.jid)
+				}
+                mentions(ht, members_id, false)
+		   	break
 				case 'antilink':
 				if (!isGroup) return reply(group())
 					if (!isGroupAdmins && !isOwner) return reply(`pp`)
